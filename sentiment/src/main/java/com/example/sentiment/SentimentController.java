4cb0d82bb4e6cba4d6796492e21723325be72dde
Josh Cummings <josh.cummings@gmail.com>
2020-11-07 09:08:38 -0700

    Look up user

--- sentiment/src/main/java/com/example/sentiment/SentimentController.java
+++ sentiment/src/main/java/com/example/sentiment/SentimentController.java
@@ -3,6 +3,7 @@ package com.example.sentiment;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,22 +13,21 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/sentiment")
 public class SentimentController {
 	private final Map<String, Sentiment> sentiment = new ConcurrentHashMap<>();
-	private final String name = "bob";
 
 	@GetMapping
-	public Sentiment read() {
-		return read(this.name);
+	public Sentiment read(Authentication authentication) {
+		return read(authentication.getName());
 	}
 
 	@PostMapping("/up")
-	public Sentiment up() {
-		Sentiment sentiment = read(this.name);
+	public Sentiment up(Authentication authentication) {
+		Sentiment sentiment = read(authentication.getName());
 		return sentiment.up();
 	}
 
 	@PostMapping("/down")
-	public Sentiment down() {
-		Sentiment sentiment = read(this.name);
+	public Sentiment down(Authentication authentication) {
+		Sentiment sentiment = read(authentication.getName());
 		return sentiment.down();
 	}
 
